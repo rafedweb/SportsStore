2069b65f72e4c6bd4e1c91d58f9ad605212e3a1a
Rafael <rafaelmendonca@outlook.com.br>
2018-03-08 08:40:17 -0300

    fix

--- SportsStore/SportsStore.UnitTests/App_Start/NinjectWebCommon.cs
+++ SportsStore/SportsStore.UnitTests/App_Start/NinjectWebCommon.cs
@@ -53,6 +53,7 @@ namespace SportsStore.UnitTests.App_Start
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
+            
         }        
     }
 }
--- SportsStore/SportsStore.UnitTests/UnitTest1.cs
+++ SportsStore/SportsStore.UnitTests/UnitTest1.cs
@@ -1,5 +1,11 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SportsStore.Domain.Abstract;
+using Moq;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Controllers;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SportsStore.UnitTests
 {
@@ -7,8 +13,29 @@ namespace SportsStore.UnitTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Can_Paginate()
         {
+            //arrage
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] {
+                  new Product {ProductID = 1, Name = "P1"},
+                  new Product {ProductID = 2, Name = "P2"},
+                  new Product {ProductID = 3, Name = "P3"},
+                  new Product {ProductID = 4, Name = "P4"},
+                  new Product {ProductID = 5, Name = "P5"}
+             });
+
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+
+            //act
+            IEnumerable<Product> result = (IEnumerable<Product>)controller.List(2).Model;
+
+            //assert
+            Product[] prodArray = result.ToArray();
+            Assert.IsTrue(prodArray.Length == 2);
+            Assert.AreEqual(prodArray[0].Name, "P4");
+            Assert.AreEqual(prodArray[1].Name, "P5");
         }
     }
 }
