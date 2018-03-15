977403bb16d7074a68fe40e9bd4d67e23faf2458
Rafael Mendonça <Rafael>
2018-03-15 00:17:21 -0300

    Finalização do carrinho de compras

--- SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -5,6 +5,7 @@ using SportsStore.Domain.Concrete;
 using SportsStore.Domain.Entities;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Web.Mvc;
 
 namespace SportsStore.WebUI.Infrastructure
@@ -35,6 +36,14 @@ namespace SportsStore.WebUI.Infrastructure
             //});
             //kernel.Bind<IProductRepository>().ToConstant(mock.Object);
             kernel.Bind<IProductRepository>().To<EFProductRepository>();
+
+            EmailSettings emailSettings = new EmailSettings
+            {
+                WriteAsFile = bool.Parse(ConfigurationManager
+                .AppSettings["Email.WriteAsFile"] ?? "false")
+            };
+            kernel.Bind<IOrderProcessor>().To<EmailOrderProcessor>()
+            .WithConstructorArgument("settings", emailSettings);
         }
     }
 }
