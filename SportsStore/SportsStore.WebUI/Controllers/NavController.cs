8e19666214b45c70187e14a6566b2c84dab4710d
Rafael Mendonça <Rafael>
2018-03-18 14:19:58 -0300

    implementaçã da responsividade para dispositivos Mobile

--- SportsStore/SportsStore.WebUI/Controllers/NavController.cs
+++ SportsStore/SportsStore.WebUI/Controllers/NavController.cs
@@ -17,7 +17,7 @@ namespace SportsStore.WebUI.Controllers
         }
 
         // GET: Nav
-        public PartialViewResult Menu(string category = null, bool horizontalLayout = false)
+        public PartialViewResult Menu(string category = null)
         {
             ViewBag.SelectedCategory = category;
 
@@ -25,8 +25,7 @@ namespace SportsStore.WebUI.Controllers
                                     .Select(x => x.Category)
                                     .Distinct()
                                     .OrderBy(x => x);
-            string viewName = horizontalLayout ? "MenuHorizontal" : "Menu";
-            return PartialView(viewName, categories);
+            return PartialView("FlexMenu", categories);
         }
     }
 }
